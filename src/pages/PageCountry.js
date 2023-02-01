7dacdeb64a3e2e9822bb6fdf934c0e29d28d88dc
HAMMER17 <plutalov70gmail.com>
2023-02-01 21:04:40 +0300

    currencies

--- src/pages/PageCountry.js
+++ src/pages/PageCountry.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import '../style/pagecountry.css'
 
 export default function PageCountry() {
@@ -11,43 +11,59 @@ export default function PageCountry() {
   const params = useParams()
 
   const [data, setData] = useState([])
-  // const [borders, setBorders] = useState([])
-  // const [country, setCountry] = useState([])
+  const [country, setCountry] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [current, setCurrent] = useState([])
+
   useEffect(() => {
     axios.get(`https://restcountries.com/v3.1/name/${params.name}`)
       .then(({ data }) => setData(data))
       .catch((err) => console.log(err + ' params.name'))
     // eslint-disable-next-line
   }, [params])
+  // console.log(params)
+  const borders = data.map(el => el.borders)
 
-  // useEffect(() => {
-  //   const borders = data.map(el => el.borders)
-  //   axios.get(`https://restcountries.com/v3.1/alpha/${borders}`)
-  //     .then(({ data }) => setCountry(data))
-  //     .then((err) => console.log(err + ' borders'))
-  // }, [params])
-  // console.log(country)
+  useEffect(() => {
+    axios.get(`https://restcountries.com/v2/name/${params.name}`)
+      .then(({ data }) => setCurrent(data))
+  }, [params])
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`https://restcountries.com/v3.1/alpha?codes=${borders}`)
+      .then(({ data }) => setCountry(data))
+      .catch((err) => console.log(err + ' borders'))
+    setLoading(false)
+  }, [data])
+
+  const array = current.map((el, i) => (<div key={i} className='current'>
+    <h3>Currencies</h3>
+    <h4><span>Name: </span>{el.currencies[0].name}</h4>
+    <h4><span>Symbol: </span>{el.currencies[0].symbol}</h4>
+  </div>))
 
   return (
     <div className='page'>
 
-      {data.map((elem, id) => {
-        return <>
-          <div className="page_left" key={id}>
+      {data.map((elem) => {
+        return <div key={elem.name.common} className="page_container">
+          <div className="page_left" key={elem.name.common}>
             <button onClick={() => navigate(-1)}>back</button>
             <img src={elem.flags.png} alt="flag" />
             <h1>{elem.name.common}</h1>
             <h2>{elem.capital}</h2>
             <h2>{elem.region}</h2>
-            <span>Population:
-              <h3>{new Intl.NumberFormat().format(elem.population)}</h3>people</span>
+            <h3><span>Population: </span> {new Intl.NumberFormat('de-DE').format(elem.population)}<span> people</span></h3>
 
           </div>
           <div className="page_right">
             <h1>{elem.name.common}</h1>
             <h3>{elem.region}</h3>
             <h3>{elem.capital}</h3>
-            <h4>{new Intl.NumberFormat('ru-RU').format(elem.population)} <span> people</span></h4>
+            <h4>{new Intl.NumberFormat('ru-RU').format(elem.population)}
+              <span> people</span></h4>
+            <div>{array}</div>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Vel, magni minima? Excepturi amet blanditiis quia illo exercitationem saepe non officia,
               mollitia est doloremque voluptatibus ex accusantium quisquam veritatis ad deserunt.</p>
@@ -55,18 +71,18 @@ export default function PageCountry() {
               Inventore id unde blanditiis eaque sapiente nostrum molestias
               rem dolore quibusdam quas eius deleniti accusantium iusto ullam,
               voluptates quidem vel nam ducimus!</p>
-            {/* <div className="page_country">
-              {country.map((el, id) => {
-                return <Link key={id} to={`/country/${el.name}`}>
-                  <h5>{el.name.common}</h5>
+            <div className="page_country">
+              {loading ? (<h1>Loading...</h1>) : country.map((el) => {
+                return <Link key={el.name.common} to={`/country/${el.name.common}`}>
+                  <h5 key={el.name.common}>{el.name.common}</h5>
                 </Link>
 
               })}
-            </div> */}
+            </div>
 
           </div>
 
-        </>
+        </div>
       })}
 
     </div>
